8830c2216e14a4a98dcb8b7527c8ce5966a01ab0
citizenchris099 <citizenchris099@gmail.com>
2015-11-17 09:12:35 -0600

    added more select menu asserts and functionality

--- practice/zoo.js
+++ practice/zoo.js
@@ -3,28 +3,71 @@
 var zooSite = 'http://thetestroom.com/jswebapp/';
 var pageTitle = 'WELCOME TO THE ZOO ADOPTION CENTER';
 var name = 'Chris Manning';
+var menuOptions = ["Please select from the drop down below","George the Turtle", "Simba the Lion", "Nemo the Fish"];
+
+/**
+ * locators
+ */
 var pg1Title = element(by.xpath("//*[contains(text(), 'WELCOME TO THE ZOO')]"));
 var nameBox = element(by.model('person.name'));
 var nameDisplay = element(by.binding('person.name'));
 var contButton = element(by.xpath("//button[@id='continue_button']"));
 var pg2Title = element(by.xpath("//*[contains(text(), 'Select you Animal')]"));
 var animalSelect = element(by.xpath("//select[@ng-model='animal']"));
+var animalOptions = element.all(by.css(".ng-pristine option"));
 
 function textCheck(element, string) {
 	expect(element.getText()).toEqual(string);
 }
 
-function chkElementPresent(element){
+function chkElementPresent(element) {
 	expect(element.isPresent()).toBe(true);
 }
 
-function isLoaded(element1, element2){
+function isLoaded(element1, element2) {
 	chkElementPresent(element1);
 	chkElementPresent(element2);
 }
 
-function useDropdown(element, optionNum){
-	element.$("[value='"+optionNum+"']").click();
+/**
+ * used to get the number of items available in a select dropdown menu
+ * 
+ * @param element :
+ *            variable of element.all
+ * @param num :
+ *            int of expected number of items
+ */
+function getNumOfDropdownOptions(element, num) {
+	element.then(function(items) {
+		expect(items.length).toBe(num);
+	});
+}
+
+function getOptionText(element, num, expectedText){
+	element.then(function(items) {
+		expect(items[num].getText()).toBe(expectedText);
+	});
+}
+
+function assertOptionText(element){
+	for(count = 0; count < menuOptions.length; count++){
+		getOptionText(element, count, menuOptions[count]);
+	}
+	
+}
+
+
+
+/**
+ * used to select an element in a select dropdown menu
+ * 
+ * @param element :
+ *            of the select dropdown
+ * @param optionNum :
+ *            string number representing the options value you wish to select
+ */
+function useDropdown(element, optionNum) {
+	element.$("[value='" + optionNum + "']").click();
 }
 
 describe('zoo practice test', function() {
@@ -36,6 +79,8 @@ describe('zoo practice test', function() {
 		textCheck(nameDisplay, name);
 		contButton.click();
 		isLoaded(pg2Title, animalSelect);
+		getNumOfDropdownOptions(animalOptions, 4);
+		assertOptionText(animalOptions);
 		useDropdown(animalSelect, "3");
 	});
 });
